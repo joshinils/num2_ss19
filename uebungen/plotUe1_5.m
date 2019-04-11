8307a47fc6f1e41e3b9b636045d3629286a4f8fd
joshinils <joshua.laskowski@web.de>
2019-04-11 12:15:35 +0200

    even more stuffs

--- uebungen/plotUe1_5.m
+++ uebungen/plotUe1_5.m
@@ -5,6 +5,15 @@ f_beta  = @(t) sin(pi/3.*t);
 
 t = [1;2;3];
 y = [2;0;-3];
-[a,b] = ue1_5(t, y);
+[alpha, beta] = ue1_5(t, y);
 
-plot(t, y);
+hold('on');
+plot(t, y, 'ro');
+
+% redefine t coarser for plotting result
+t = t(1)-1 : .1 : t(end)+1;
+
+fun = @(t) alpha * f_alpha(t) + beta * f_beta(t);
+
+% plot result
+plot(t, fun(t), 'g');
