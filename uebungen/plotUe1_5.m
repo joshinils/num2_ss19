49e7558663c3700471bfdf7559e559abb92e8d5a
joshinils <joshua.laskowski@web.de>
2019-04-17 08:52:17 +0200

    more configurable ue solution

--- uebungen/plotUe1_5.m
+++ uebungen/plotUe1_5.m
@@ -3,28 +3,31 @@
 f_alpha = @(t) cos(pi/4.*t);
 f_beta  = @(t) sin(pi/3.*t);
 
-t = [1;2;3];
-y = [2;0;-3];
+t_anz = 1e3;
+t = linspace(1, 30, t_anz)';
+y = 42 * cos(pi/4.*t) + 5 * sin(pi/3.*t) + randn(length(t), 1)*50;
 
 A = zeros(length(t), 2);
-for i = 1:length(t)
-	A(i,:) = [f_alpha(t(i)), f_beta(t(i))];
-end
+A(1:length(t), :) = [f_alpha(t), f_beta(t)];
 
 % % test error
 % A=[1,5;1,5;1,5];
 
-x = linAusQR(A, y);
-%x = linAusTransA(A, y);
+x = linAusQR(A, y)
+x_tr = linAusTransA(A, y)
 alpha = x(1); beta =x(2);
 
-hold('on');
-plot(t, y, 'ro');
+% do plot?
+if true
+	fun = @(t) alpha * f_alpha(t) + beta * f_beta(t);
 
-% redefine t coarser for plotting result
-t = t(1)-1 : .1 : t(end)+1;
+	hold('on');
+	plot(t, y, 'ro');
 
-fun = @(t) alpha * f_alpha(t) + beta * f_beta(t);
+	% redefine t coarser for plotting result
+	t = t(1)-1 : .1 : t(end)+1;
 
-% plot result
-plot(t, fun(t), 'g');
+	% plot result
+	plot(t, fun(t), 'g','LineWidth', 2);
+end
+
